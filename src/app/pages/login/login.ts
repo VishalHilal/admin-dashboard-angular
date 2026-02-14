3c4d94effc0e647f3d4d217455323f09757d2cf5
VishalHilal <vishalschool24@gmail.com>
2026-02-14 22:17:07 +0530

    login page modified

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
+  email = '';
+  password = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.email === 'admin' && this.password === '123') {
+      alert('Login Successful');
+      this.router.navigate(['/dashboard']);
+    } else {
+      alert('Invalid credentials');
+    }
+  }
 }
