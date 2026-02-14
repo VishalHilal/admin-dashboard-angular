343169d274ac18e711a252bc3fc4f07926115491
VishalHilal <vishalschool24@gmail.com>
2026-02-14 22:09:31 +0530

    login and dashboard created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {Dashboard} from './pages/dashboard/dashboard';
+import {Login} from './pages/login/login'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{path:'', component:Dashboard},
+	{path:'login', component:Login}
+
+];
